8ae9ecb8d823170acee7f8db596acdf4ebf213e6
chiduralasreeja@gmail.com <chiduralasreeja@gmail.com>
2025-06-21 12:11:17 +0530

    shell

---
+++ mongodb.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$( id -u )
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER=/var/log/shellscript-logs
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+echo "creating $LOGS_FOLDER"
+
+
+mkdir -p $LOGS_FOLDER # -p --> It creates parent directories as needed (e.g., if /var/log/myapp/logs doesn’t exist, it will create the full path).
+echo "script started executed at : $(date)" | tee -a  $LOG_FILE 
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R ERROR: Please run with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else    
+    echo -e "$G you are running with root access $N" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then    
+        echo -e "$G $2 is.... SUCCESS $N " | tee -a $LOG_FILE
+    else
+        echo  -e " $R installing $2 is failure $N" | tee -a $LOG_FILE
+        exit 1 #give other than 0 upto 127
+    fi
+}
+
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+
+
+
+dnf install mongodb-org -y 
+VALIDATE $? "installing mongodb"
+
+systemctl enable mongod 
+VALIDATE $? " enabling mongodb"
+
+systemctl start mongod
+VALIDATE $? "starting mongodb"
