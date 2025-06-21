6f34da775ee1276f654627db5c22b784a277f89d
chiduralasreeja@gmail.com <chiduralasreeja@gmail.com>
2025-06-21 11:53:23 +0530

    shell

--- roboshop.sh
+++ roboshop.sh
@@ -8,7 +8,8 @@ ZONE_ID="Z02370613NFA2YD1CKRZ2"
 DOMAIN_NAME="sreeja.site"
 SUBNET_ID="$subnet-00af09fb15e458af9"
 
-for instance in ${INSTANCES[@]}
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
     instance_id=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-0672a218cab9ebbb0 --subnet-id subnet-00af09fb15e458af9 --network-interfaces "DeviceIndex=0,SubnetId=$SUBNET_ID,AssociatePublicIpAddress=true,Groups=$SG_ID" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
 
