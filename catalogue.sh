ea88b0e5f6b8a1370ac7a5ea798b1ac9e61de062
chiduralasreeja@gmail.com <chiduralasreeja@gmail.com>
2025-06-23 08:50:32 +0530

    shell

--- catalogue.sh
+++ catalogue.sh
@@ -91,7 +91,7 @@ VALIDATE $? "installing mongodb"
 STATUS=$(mongosh --host mongodb.daws84s.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.daws84s.site </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.sreeja.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
