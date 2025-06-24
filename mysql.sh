b4b3ba70f13714919cc982b6a3793284a8350bdf
chiduralasreeja@gmail.com <chiduralasreeja@gmail.com>
2025-06-24 22:18:43 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -47,7 +47,7 @@ read -s MYSQL_ROOT_PASSWORD
 mysql_secure_installation --set-root-pass read -p $MYSQL_ROOT_PASSWORD
 
 
-END_TIME= $(date +%s)
+END_TIME= $(date+%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
