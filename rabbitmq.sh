6ad428c0f9c93f84f3264c6b7f4cf2b6d6f9dabb
chiduralasreeja@gmail.com <chiduralasreeja@gmail.com>
2025-06-25 22:47:54 +0530

    shell

---
+++ rabbitmq.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+START_TIME= $(date +%s)
+USERID=$( id -u )
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER=/var/log/shellscript-logs
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+echo "creating $LOGS_FOLDER"
+
+
+mkdir -p $LOGS_FOLDER # -p --> It creates parent directories as needed (e.g., if /var/log/myapp/logs doesn’t exist, it will create the full path).
+echo "script started executed at : $(date)" | tee -a  $LOG_FILE 
+
+if [ $USERID -ne 0 ]
+then 
+    echo -e "$R ERROR: Please run with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else    
+    echo -e "$G you are running with root access $N" | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then    
+        echo -e "$G $2 is.... SUCCESS $N " | tee -a $LOG_FILE
+    else
+        echo  -e " $R  $2 is failure $N" | tee -a $LOG_FILE
+        exit 1 #give other than 0 upto 127
+    fi
+}
+
+dnf install rabbitmq-server -y &>>$LOG_FILE
+VALIDATE $? "installing rabbitmq"
+
+
+systemctl enable rabbitmq-server
+VALIDATE $? "enabling rabbitmq"
+
+systemctl start rabbitmq-server
+VALIDATE $? "starting rabbitmq"
+
+
+rabbitmqctl add_user roboshop roboshop123 
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+END_TIME= $(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
